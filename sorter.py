fef977b41c98ee0afb5b493dc2d272831d414592
PavelMeld <pavel.meld@gmail.com>
2013-05-15 20:39:12 +0400

    Initial commit

---
+++ sorter.py
@@ -0,0 +1,14 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+def get_exif(fn):
+    ret = {}
+    i = Image.open(fn)
+    info = i._getexif()
+    for tag, value in info.items():
+        decoded = TAGS.get(tag, tag)
+        ret[decoded] = value
+    return ret
+
+info = get_exif("2013/02/21/IMG_20130221_200550.jpg");
+print info['DateTimeOriginal']
