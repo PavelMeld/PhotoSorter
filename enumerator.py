a391e97961dfc9c946e64368e1a8a12ef1e2417e
PavelMeld <pavel.meld@gmail.com>
2019-05-18 10:40:09 +0300

    Changed enumeration routine

--- enumerator.py
+++ enumerator.py
@@ -2,28 +2,72 @@
 import os
 import re
 
-def enumerate_by_mask(root, mask, what="files"):
-    res = [];
-    result = os.walk(root)
+g_cache = {};
 
-    for diritem in result:
-        (search_path, dir_list, file_list) = diritem
-
-        if what == "files":
-            items = file_list;
-        if what == "dirs":
-            items = dir_list;
-
-        for filename in items:
-            if not re.search(mask, filename, re.I):
-                continue;
-            full_path = os.path.join(diritem[0], filename);
-            res.append(full_path);
-    return res;
-
-def enumerate_similar_dirs(root, dirname):
-    return enumerate_by_mask(root, dirname+".*", "dirs");
+def enumerate_by_mask_cached(cached, mask, what="files", verbose=False):
+	res = []
+	for filename in cached:
+		if not re.search(mask, filename, re.I):
+			continue;
+		res.append(filename);
+	return res;
 
 
-def enumerate_media_files(root):
-    return enumerate_by_mask(root, '\.(:?(jpg)|(mov)|(avi)|(mp4)|(mts))$');
+def enumerate_by_mask(root, mask, what="files", verbose=False):
+	res = [];
+
+	if verbose:
+		print "Getting file list at "+root,
+	result = os.walk(root)
+	if verbose:
+		print " done"
+
+	if verbose:
+		print "Sorting items ",
+
+	cache = [];
+
+	for diritem in result:
+		(search_path, dir_list, file_list) = diritem
+
+		if what == "files":
+			items = file_list;
+		if what == "dirs":
+			items = dir_list;
+
+		for filename in items:
+			full_path = os.path.join(diritem[0], filename);
+			cache.append(full_path);
+			if not re.search(mask, filename, re.I):
+				continue;
+			res.append(full_path);
+
+	if verbose:
+		print " done"
+
+	return (res, cache);
+
+
+def enumerate_similar_dirs(root, dirname, verbose=False):
+	if g_cache.has_key(root):
+		if verbose:
+			print "Found cached "+root;
+		return enumerate_by_mask_cached(g_cache[root], dirname+".*", "dirs", verbose)
+	
+	if verbose:
+		print "Wait a minute, filling directory cache ..."
+
+	(res, cache) = enumerate_by_mask(root, dirname+".*", "dirs", verbose);
+	g_cache[root] = cache;
+
+	return res;
+
+
+def update_similar_dirs_cache(root, dirname, verbose=False):
+	if g_cache.has_key(root) == True and not dirname in g_cache[root]:
+		g_cache[root].append(dirname);
+
+
+
+def enumerate_media_files(root, verbose=False):
+	return enumerate_by_mask(root, '\.(:?(jpg)|(mov)|(avi)|(mp4)|(mts))$', "files", verbose)[0]
--- sorter.py
+++ sorter.py
@@ -12,19 +12,19 @@ import mtsdate
 import argparse
 
 def getHumanSize(byteSize):
-    if byteSize < 1024: 
-        suffix = 'b';
-        div = 1;
-    elif byteSize < 1024 * 1024:
-        suffix = 'kb'
-        div = 1024
-    elif byteSize < 1024 *1024*1024:
-        suffix = 'Mb';
-        div = 1024 * 1024;
-    else:
-        suffix = 'Gb';
-        div = 1024 * 1024 * 1024
-    return '{:.2f}{}'.format(1.0 * byteSize/div, suffix)
+	if byteSize < 1024: 
+		suffix = 'b';
+		div = 1;
+	elif byteSize < 1024 * 1024:
+		suffix = 'kb'
+		div = 1024
+	elif byteSize < 1024 *1024*1024:
+		suffix = 'Mb';
+		div = 1024 * 1024;
+	else:
+		suffix = 'Gb';
+		div = 1024 * 1024 * 1024
+	return '{:.2f}{}'.format(1.0 * byteSize/div, suffix)
 
 ################################################################################
 #
@@ -34,24 +34,24 @@ def getHumanSize(byteSize):
 #
 ################################################################################
 def file_exists(dest_dir, fname, fext, fsize):
-    suff = "";
-    n = 0;
-    while True:
-        testdest = os.path.join(dest_dir, fname + suff + fext)
+	suff = "";
+	n = 0;
+	while True:
+		testdest = os.path.join(dest_dir, fname + suff + fext)
 
-        if os.path.exists(testdest) == False:
-            return False;
+		if os.path.exists(testdest) == False:
+			return False;
 
 
-        dsize = os.path.getsize(testdest)
-        if fsize == dsize:
-            print "File exists";
-            return True;
+		dsize = os.path.getsize(testdest)
+		if fsize == dsize:
+			print "File exists";
+			return True;
 
-        suff = "_" + str(n);
-        n = n + 1;
+		suff = "_" + str(n);
+		n = n + 1;
 
-    return False;
+	return False;
 
 ################################################################################
 #
@@ -60,42 +60,44 @@ def file_exists(dest_dir, fname, fext, fsize):
 #
 #
 ################################################################################
-def get_candidate_dirs(root, dirname):
-    return enumerator.enumerate_similar_dirs(root, dirname);
+def get_candidate_dirs(root, dirname, verbose = False):
+	return enumerator.enumerate_similar_dirs(root, dirname, verbose);
 
 ################################################################################
 #
 #
-#       Get EXIF using PIL
+#		Get EXIF using PIL
 #
 #
 ################################################################################
 def get_exif(fn):
-    ret = {}
-    i = Image.open(fn)
-    try:
-        info = i._getexif();
-    except AttributeError:
-        return None
-            
-    if info == None:
-        return ret
-    for tag, value in info.items():
-        decoded = TAGS.get(tag, tag)
-        ret[decoded] = value
+	ret = {}
+	i = Image.open(fn)
+	try:
+		info = i._getexif();
+	except :
+		return None
+			
+	if info == None:
+		return None;
+	for tag, value in info.items():
+		decoded = TAGS.get(tag, tag)
+		ret[decoded] = value
 
-    if ret.has_key('DateTimeOriginal')==False:
-        return None;
+	if ret.has_key('DateTimeOriginal')==False:
+		return None;
 
-    res = ret['DateTimeOriginal'];
+	res = ret['DateTimeOriginal'];
 
-    res = re.match('^([^\s]+)\s', res);
-    if res == None:
-        return None;
+	print "jdt", res,
 
-    res = re.sub(':', '_', res.group(1))
-    res = re.sub('\000', '_', res)
-    return res
+	res = re.match('^([^\s]+)\s', res);
+	if res == None:
+		return None;
+
+	res = re.sub(':', '_', res.group(1))
+	res = re.sub('\000', '_', res)
+	return res
 
 
 cmd = argparse.ArgumentParser(description='PhotoSorter')
@@ -104,98 +106,130 @@ cmd.add_argument("source");
 cmd.add_argument("destination");
 
 args = cmd.parse_args();
-print args
 
-source          = args.source;
-destination     = args.destination
-mode            = args.mode;
+source			= args.source;
+destination		= args.destination
+mode			= args.mode;
+test			= False;
 
-print "Source      path '",source,"'";
+if mode != 'copy':
+	test = True;
+
+print "Source	   path '",source,"'";
 print "Destination path '",destination,"'";
 
 
 if os.path.exists(source) == False:
-    print "Source dir", source," not exists!"
-    sys.exit();
+	print "Source dir", source," not exists!"
+	sys.exit();
 
 if mode != 'test':
-    if os.path.exists(destination) == False:
-        print "Destination dir", destination," not exists!"
-        sys.exit();
+	if os.path.exists(destination) == False:
+		print "Destination dir", destination," not exists!"
+		sys.exit();
 
 totalBytes = 0
 
 
-for filename in enumerator.enumerate_media_files(source):
-    name = os.path.basename(filename);
-    file_size = os.path.getsize(filename)
-    totalBytes += file_size
-    namestart = os.path.splitext(name)[0]
-    ext = os.path.splitext(name)[1]
+for filename in enumerator.enumerate_media_files(source, verbose=test):
 
-    info = None;
+	if test:
+		print "Reading info of " + filename,
 
-    if re.search('\.jpg$',ext,re.I):
-        try:
-            info = get_exif(filename)
-        except IOError:
-            info = None;
-    elif re.search('\.avi$',ext,re.I):
-        info = avidate.read_avi_date(filename)
-    elif re.search('\.mov$',ext,re.I):
-        info = movdate.read_mov_date(filename)
-    elif re.search('\.mp4$',ext,re.I):
-        info = mp4date.read_mp4_date(filename)
-    elif re.search('\.mts$',ext,re.I):
-        info = mtsdate.read_mts_date(filename)
+	name = os.path.basename(filename)
+	file_size = os.path.getsize(filename)
+	totalBytes += file_size
+	namestart = os.path.splitext(name)[0]
+	ext = os.path.splitext(name)[1]
 
-    if info == None:
-        dst = 'NOT_DATED';
-    elif len(info) == 0:
-        dst = 'NOT_DATED';
-    else:
-        dst = info;
+	info = None;
 
+	#
+	# Essential date extraction
+	#
+	if re.search('\.jpg$',ext,re.I):
+		try:
+			info = get_exif(filename)
+		except IOError:
+			info = None;
+	elif re.search('\.avi$',ext,re.I):
+		info = avidate.read_avi_date(filename)
+	elif re.search('\.mov$',ext,re.I):
+		info = movdate.read_mov_date(filename)
+	elif re.search('\.mp4$',ext,re.I):
+		info = mp4date.read_mp4_date(filename)
+	elif re.search('\.mts$',ext,re.I):
+		info = mtsdate.read_mts_date(filename)
+	
+	
+	# 
+	# Name-based date extraction
+	#
 
-    default_dest_dir = os.path.join(destination, dst)
-    candidates       = get_candidate_dirs(destination, dst);
-    candidates.append(default_dest_dir);
+	if info == None or info == "1904_01_01":
+		whatsapp = re.search('(?:img|vid)-(\d{4})(\d{2})(\d{2})-wa', filename, re.I) 
+		if whatsapp:
+			info = whatsapp.group(1) + "_" + whatsapp.group(2) + "_" + whatsapp.group(3);
+	
 
-    print filename,
+	#
+	# If still nothing
+	#
+	if info == None:
+		dst = 'NOT_DATED';
+	elif len(info) == 0:
+		dst = 'NOT_DATED';
+	else:
+		dst = info;
 
-    if mode == 'test':
-        print " testing", 
+	if test:
+		print " done, dst is ", dst;
 
-    for testdir in candidates:
-        if file_exists(testdir, namestart, ext, file_size):
-            print "found in " + testdir;
-            break;
-    else:
-        # Copying
-        fulldest = os.path.join(default_dest_dir, name)
+	if test:
+		print "Getting candidate list",
+	default_dest_dir = os.path.join(destination, dst)
+	candidates		 = get_candidate_dirs(destination, dst, test);
+	candidates.append(default_dest_dir);
 
-        if mode == 'test':
-            print " skipped"
-            continue;
+	if test:
+		print "done",
 
-        print '->',fulldest, '(', getHumanSize(file_size),')'
+	print filename,
 
-        if os.path.exists(default_dest_dir) == False:
-            print 'Creating directory ... '+default_dest_dir
-            os.mkdir(default_dest_dir)
+	if mode == 'test':
+		print " testing", 
 
-        n = 0
-        while True:
-            if os.path.exists(fulldest) == False:
-                print 'Copying...'
-                shutil.copy(filename, fulldest)
-                break;
+	for testdir in candidates:
+		if file_exists(testdir, namestart, ext, file_size):
+			print "found in " + testdir;
+			break;
+	else:
+		# Copying
+		fulldest = os.path.join(default_dest_dir, name)
 
-            fulldest = os.path.join(default_dest_dir, name)
-            fulldest = os.path.join(default_dest_dir, namestart+"_"+str(n)+ext)
-            print 'File name is busy, trying', fulldest
+		if mode == 'test':
+			print " skipped"
+			continue;
 
-            n = n + 1
+		print '->',fulldest, '(', getHumanSize(file_size),')'
+
+		if os.path.exists(default_dest_dir) == False:
+			print 'Creating directory ... '+default_dest_dir
+			os.mkdir(default_dest_dir)
+			enumerator.update_similar_dirs_cache(destination, dst);
+
+		n = 0
+		while True:
+			if os.path.exists(fulldest) == False:
+				print 'Copying...'
+				shutil.copy(filename, fulldest)
+				break;
+
+			fulldest = os.path.join(default_dest_dir, name)
+			fulldest = os.path.join(default_dest_dir, namestart+"_"+str(n)+ext)
+			print 'File name is busy, trying', fulldest
+
+			n = n + 1
 
 
 print 'Total', totalBytes,' (', getHumanSize(totalBytes),')'
